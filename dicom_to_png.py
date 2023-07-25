3a53f4afab88080b119f10472384489028ca3c64
Yehyun <yehyun.suh@vanderbilt.edu>
2023-07-25 14:07:17 -0500

    [Bug Fix] Depth Generalization #1

---
+++ dicom_to_png.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pydicom
+import argparse
+import os
+
+from PIL import Image
+from glob import glob
+from tqdm import tqdm
+
+
+def main(args):
+    path_list, dicom_path_list = [], []
+    path_list.append(args.dicom_folder)
+
+    # Create png folder
+    if not os.path.exists(f'./{args.png_folder}'):        
+        os.mkdir(f'./{args.png_folder}')
+    
+    # Search for path to all dicom files
+    while path_list != []:
+        current_path = path_list.pop()
+        tmp_path_list = glob(f'{current_path}/*')
+
+        if tmp_path_list != []:
+            for tmp_path in tmp_path_list:
+                path_list.append(tmp_path)
+        else:
+            dicom_path_list.append(current_path)
+
+    # From the paths found, convert them in to png format
+    for dicom_name in tqdm(dicom_path_list):
+        try:
+            dcm_info = pydicom.read_file(f'{dicom_name}', force=True)
+            dcm_img = dcm_info.pixel_array
+            split = dicom_name.split('/')
+
+            file_name = ''
+            for i in range(len(split)):
+                file_name += f'{split[(i-len(split)+1)]}'
+                if (i-len(split)+1) < -1:
+                    file_name += '_'
+                elif (i-len(split)+1) == -1:
+                    break
+            norm = (dcm_img - np.min(dcm_img)) / (np.max(dcm_img) - np.min(dcm_img))
+            norm_img = np.uint8(norm*255)
+            show_img = Image.fromarray(norm_img)
+            show_img.save(f'./{args.png_folder}/{file_name}.png')
+        except: 
+            print(f'This file {dicom_name} did not convert to PNG format')
+            
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dicom_folder', type=str, default="./dicom_files")
+    parser.add_argument('--png_folder', type=str, default="./png")
+    args = parser.parse_args()
+
+    main(args)
