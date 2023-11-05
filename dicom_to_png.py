fff51877ad3a1c1b9b64a57b47ac61f49557c13e
Yehyun <yehyun.suh@vanderbilt.edu>
2023-11-05 12:31:20 -0600

    Save png in external ssd

--- dicom_to_png.py
+++ dicom_to_png.py
@@ -13,9 +13,8 @@ def main(args):
     path_list.append(args.dicom_folder)
 
     # Create png folder
-    if not os.path.exists(f'./{args.png_folder}'):        
-        os.mkdir(f'./{args.png_folder}')
-    
+    os.makedirs(args.png_folder, exist_ok=True)
+
     # Search for path to all dicom files
     while path_list != []:
         current_path = path_list.pop()
@@ -33,26 +32,25 @@ def main(args):
             dcm_info = pydicom.read_file(f'{dicom_name}', force=True)
             dcm_img = dcm_info.pixel_array
             split = dicom_name.split('/')
+            file_name = f'{split[1]}'
 
-            file_name = ''
-            for i in range(len(split)):
-                file_name += f'{split[(i-len(split)+1)]}'
-                if (i-len(split)+1) < -1:
-                    file_name += '_'
-                elif (i-len(split)+1) == -1:
+            for i in range(2, len(split)):
+                if split[i] == args.dicom_folder:
                     break
-            norm = (dcm_img - np.min(dcm_img)) / (np.max(dcm_img) - np.min(dcm_img))
+                file_name += f'_{split[i]}'
+
+            norm = (dcm_img-np.min(dcm_img))/(np.max(dcm_img)-np.min(dcm_img))
             norm_img = np.uint8(norm*255)
             show_img = Image.fromarray(norm_img)
-            show_img.save(f'./{args.png_folder}/{file_name}.png')
-        except: 
+            show_img.save(f'{args.png_folder}/{file_name}.png')
+        except:
             print(f'This file {dicom_name} did not convert to PNG format')
-            
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dicom_folder', type=str, default="./dicom_files")
-    parser.add_argument('--png_folder', type=str, default="./png")
+    parser.add_argument('--dicom_folder', type=str, default="dicom_files")
+    parser.add_argument('--png_folder', type=str, default="png")
     args = parser.parse_args()
 
     main(args)
